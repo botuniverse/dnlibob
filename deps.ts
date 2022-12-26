4c41ce83bb7604cb68a8a6321b1864ff06707232
idanran <idanran@outlook.com>
2022-12-26 18:45:56 +0800

    0.15.2

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { Logger } from 'https://deno.land/x/reggol_deno@1.3.3/mod.ts'
 // @deno-types="https://deno.land/x/msgpackr@v1.8.0/index.d.ts"
 export { pack, unpack } from 'https://deno.land/x/msgpackr@v1.8.0/index.js'
-export { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
+export { serve } from 'https://deno.land/std@0.170.0/http/server.ts'
